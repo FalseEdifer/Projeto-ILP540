041f0a2298b7f8d1c810b5e7a1e8149a3240e051
FalseEdifer <100363784+FalseEdifer@users.noreply.github.com>
2022-03-24 12:24:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,38 +9,55 @@
     <link rel="stylesheet" href="index.css">
     <title>Test</title>
 </head>
-<div class="gif">
 <body>
-    <header>
-        <div> 
-            <ul id="form">
-                <li><a href="#Login" accesskey="1">[1] Login</a></li>
-                <li><a href="#Menu" accesskey="2">[2] Menu</a></li>
-                <li><a href="#Principal" accesskey="3">[3] Principal</a></li>
-                <li><a href="#rodape" accesskey="4">[4] Rodape</a></li>              
-            </ul>          
-       </div>
-    </header>
     <main>
-        <div  class="login" id="Login">
-            <div id="centro">
-                <img id="img" src="IMG/sunset-fuji-mountain copiar.png">
-                <form action="#" method="post">
-                    <p><label><input type="text" name="usuario" id="usuario" placeholder="Usuario"/></label></p>
-                    <p><label><input type="password" name="Senha" id="senha" placeholder="Senha"/></label></p>
-                    <p><button>Entrar</button></p>
-                    <a href="Cadastro.html" id="text">Não tem conta ainda ? Clique aqui!! </a>
-                </form>
+        <div>
+        <div class="float-lg-none">
+            <div  class="login container-fluid" id="Login">
+                <div>
+                    <div>
+                         <img id="img" src="IMG/sunset-fuji-mountain copiar.png">
+                    </div>
+                    <form action="#" method="post">
+                        <p><label><input type="text" name="usuario" id="usuario" placeholder="Usuario"/></label></p>
+                        <p><label><input type="password" name="Senha" id="senha" placeholder="Senha"/></label></p>
+                        <p><button>Entrar</button></p>
+                        <a href="Cadastro.html" id="text">Não tem conta ainda ? Clique aqui!! </a>
+                    </form>
+                </div>
             </div>
-        </div>
-        <nav id="menu">
-            <ul id="lc">
-                <li><a href="intex.html">Home</a></li>
-                <li><a href="Contato.html">Contato</a></li>
-            </ul>
-
-        </nav>
-    </main>
+        </div>   
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br><br>
+            <br>
+            <br><br>
+            <br>
+            <br><br>
+            <br>
+            <br><br>
+            <br>
+            <br><br>
+            <br>
+            <br><br>
+            <br>
+            <br><br>
+            <br>
+            <br><br>
+            <br>
+            <br><br>
+            <br>
+            <br><br>
+            <br>
+            <br><br>
+            <br>
+            <br>
+        </main>
+    </div>  
+    </main> 
         <script src="js/jquery-3.3.1.slim.min.js"></script>
         <script src="js/popper.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
